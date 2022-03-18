fe187538179928325941ad18d5e27447a1c21798
Elis4Torres <101807222+Elis4Torres@users.noreply.github.com>
2022-03-18 12:07:30 -0300

    Add files via upload

---
+++ exercicio2_emilynn_elis/src/exercicio2_emilynn_elis/Exercicio2_emilynn_elis.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio2_emilynn_elis;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 2etimds
+ */
+public class Exercicio2_emilynn_elis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner ent = new Scanner (System.in);
+        
+        double num, soma = 0;
+        int cont = 0;
+        
+        do {
+            System.out.println("Digite um número: ");
+            num = ent.nextDouble(); 
+            
+            if(num >= 0){
+                soma = num + soma;
+                cont++;
+            }
+            break;
+        } while(num>=0);
+        
+        System.out.println("A soma é " + soma);
+        System.out.println("A quantidade de números digitados foi " + cont);
+        System.out.println("A média é " + soma / cont);
+    }    
+}
