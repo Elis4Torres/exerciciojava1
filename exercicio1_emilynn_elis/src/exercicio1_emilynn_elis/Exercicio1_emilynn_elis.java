806fe157ed1468a267f80eba9aec4af8f0178fc1
Elis4Torres <101807222+Elis4Torres@users.noreply.github.com>
2022-03-24 10:32:47 -0300

    Add files via upload

---
+++ exercicio1_emilynn_elis/src/exercicio1_emilynn_elis/Exercicio1_emilynn_elis.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio1_emilynn_elis;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 2etimds
+ */
+public class Exercicio1_emilynn_elis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner ent = new Scanner (System.in);
+        
+        int uni;
+        int deze;
+        int cente;
+        int result;
+        
+         System.out.println("Digite um número: ");
+         
+         result = ent.nextInt();
+         
+         String[] unidade={"um", "dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove"};
+         String[] dezena={"dez", "vinte", "trinta", "quarenta", "cinquenta","sessenta","setenta", "oitenta", "noventa"};
+         String[] centena= {"cem", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seissentos", "setessentos", "oitossentos", "novessentos"}; 
+         
+         
+    }
+    }
