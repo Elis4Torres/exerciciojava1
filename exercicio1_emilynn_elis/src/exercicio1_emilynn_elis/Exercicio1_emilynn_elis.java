22cfec7f48cd73db9bc6ac54fa99fa33c7c3ef35
Elis4Torres <101807222+Elis4Torres@users.noreply.github.com>
2022-03-25 10:33:29 -0300

    Add files via upload

--- exercicio1_emilynn_elis/src/exercicio1_emilynn_elis/Exercicio1_emilynn_elis.java
+++ exercicio1_emilynn_elis/src/exercicio1_emilynn_elis/Exercicio1_emilynn_elis.java
@@ -17,21 +17,26 @@ public class Exercicio1_emilynn_elis {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner ent = new Scanner (System.in);
-        
-        int uni;
-        int deze;
-        int cente;
-        int result;
-        
-         System.out.println("Digite um número: ");
-         
-         result = ent.nextInt();
-         
-         String[] unidade={"um", "dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove"};
-         String[] dezena={"dez", "vinte", "trinta", "quarenta", "cinquenta","sessenta","setenta", "oitenta", "noventa"};
-         String[] centena= {"cem", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seissentos", "setessentos", "oitossentos", "novessentos"}; 
-         
-         
+        um();
+              
     }
+    
+    public static void extenso(String a, int b) {
+    
+    String[][] unidade={
+    {"dez", "zero","",""},
+    {"onze", "um","cento"},
+    {"doze", "dois","vinte","duzentos"},
+    {"treze", "três","trinta","trezentos"},
+    {"quatorze", "quatro","quarenta","quatrocentos"},
+    {"quinze", "cinco","cinquenta","quinhentos"},
+    {"dezesseis", "seis","sessenta","seiscentos"},
+    {"dezessete", "sete","setenta","setecentos"},
+    {"dezoito", "oito","oitenta","oitocentos"},
+    {"dezenove", "nove","noventa","novecentos"},
+    };
+    
+        System.out.print(unidade[ Integer.parseInt])
     }
+    
+}
